6fe523376f45df06b4e2be32994b7fb6c126b2ac
a805431 <gergana.ivanova@yara.com>
2023-03-31 11:14:36 +0300

    s6v65 and 66

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        // autoCapitalize and autoCorrect props added, so input is not capitalized and spellchecked in iOS
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>My name is: {name}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
