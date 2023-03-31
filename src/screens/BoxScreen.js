cd006f1e578fe2a0774375cfc86cfe3999843095
a805431 <gergana.ivanova@yara.com>
2023-03-31 15:06:53 +0300

    s7v72

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
@@ -13,11 +15,14 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
+    //alignItems: 'flex-end' //или 'flex-start', или 'center', default is stretch
+    flexDirection: 'row',
+    height: 200,
+    alignItems: 'flex-start'
   },
   textStyle: {
-    borderWidth: 10,
+    borderWidth: 3,
     borderColor: 'red',
-    margin: 20
   },
 });
 
