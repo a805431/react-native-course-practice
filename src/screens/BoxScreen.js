e6a75b0fffaf64b75eeafe44fb44b0ac0aa52989
a805431 <gergana.ivanova@yara.com>
2023-03-31 15:43:46 +0300

    s7v74-75 flex properties in the child container

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
@@ -16,13 +16,28 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'black',
     //alignItems: 'flex-end' //или 'flex-start', или 'center', default is stretch
-    flexDirection: 'row',
+    // flexDirection: 'row',
     height: 200,
-    justifyContent: 'flex-end'//'flex-start'-default'center', 'space-between', 'space-around'
+    // justifyContent: 'flex-end'//'flex-start'-default'center', 'space-between', 'space-around'
+    alignItems: 'center'
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
+    // flex: 4
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    //flex: 1 causes child 2 to expand and take up as much vertical space as it possibly can
+    //flex operates in the same direction as the current value of flexDirection
+    // flex: 4
+    alignSelf: 'stretch'//'center', 'flex-start' etc., the same as alignItems property for the parent
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    // flex: 2
   },
 });
 
