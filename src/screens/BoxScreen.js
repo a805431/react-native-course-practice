17e2f290201418fc9a5b1da1d9fd673d11a7d24b
a805431 <gergana.ivanova@yara.com>
2023-03-31 17:16:24 +0300

    s7v80-81

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,35 +3,38 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child #1</Text>
-      <Text style={styles.textTwoStyle}>Child #2</Text>
-      <Text style={styles.textThreeStyle}>Child #3</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
     borderColor: 'black',
     //alignItems: 'flex-end' //или 'flex-start', или 'center', default is stretch
-    // flexDirection: 'row',
+    flexDirection: 'row', //'row' or 'column', which is the default
     height: 200,
     // justifyContent: 'flex-end'//'flex-start'-default'center', 'space-between', 'space-around'
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
   },
-  textOneStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
+  viewOneStyle: {
+    height: 50,
+    width: 50,
     // flex: 4
+    backgroundColor: 'red',
   },
-  textTwoStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
     //flex: 1 causes child 2 to expand and take up as much vertical space as it possibly can
     //flex operates in the same direction as the current value of flexDirection
     // flex: 4
-    //alignSelf: 'stretch'//'center', 'flex-start' etc., the same as alignItems property for the parent
+    //alignSelf: 'stretch'//'center', 'flex-start' etc., the same as alignItems property for the parent, alignSelf overwrites it
     // position: 'absolute', //'relative' is the default value
     //left: 10 //also, top, bottom, right for added padding in these directions
 
@@ -41,12 +44,15 @@ const styles = StyleSheet.create({
     // right: 0,
     // bottom: 0,
     // left: 0
-    ...StyleSheet.absoluteFillObject,
+    // ...StyleSheet.absoluteFillObject,
+    marginTop: 50, //стана и с top: 50
+    backgroundColor: 'green',
   },
-  textThreeStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
-    // flex: 2
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    // flex: 2,
+    backgroundColor: 'purple',
   },
 });
 
